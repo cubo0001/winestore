bc644a3c441063003d3330f730794c6793852d60
thanhtung <tranthanhtung0709@gmail.com>
2017-07-31 03:33:48 +0700

    Fixed: price in product detail

--- wp-content/plugins/woocommerce/templates/single-product/price.php
+++ wp-content/plugins/woocommerce/templates/single-product/price.php
@@ -25,7 +25,7 @@ global $product;
 ?>
 <div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 
-	<p class="price"><?php echo $product->get_price_html(); ?></p>
+	<p class="price">Liên hệ: 098 221 6068</p>
 
 	<meta itemprop="price" content="<?php echo esc_attr( $product->get_display_price() ); ?>" />
 	<meta itemprop="priceCurrency" content="<?php echo esc_attr( get_woocommerce_currency() ); ?>" />
