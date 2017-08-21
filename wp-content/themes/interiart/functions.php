798bb2a96c040266bc35afe33864596150c9a69f
thanhtung <tranthanhtung0709@gmail.com>
2017-08-21 16:54:07 +0700

    --amend

--- wp-content/themes/interiart/functions.php
+++ wp-content/themes/interiart/functions.php
@@ -1124,7 +1124,7 @@ add_filter( 'woocommerce_output_related_products_args', 'bbloomer_change_number_
 
 function bbloomer_change_number_related_products( $args ) {
 
-    $args['posts_per_page'] = 4; // # of related products
+    $args['posts_per_page'] = 6; // # of related products
     $args['columns'] = 8; // # of columns per row
     return $args;
 }
