821e097661618630885d6c2e1ab97b584739b122
thanhtung <tranthanhtung0709@gmail.com>
2017-08-21 16:38:02 +0700

    --amend

--- wp-content/themes/interiart/functions.php
+++ wp-content/themes/interiart/functions.php
@@ -1119,4 +1119,13 @@ if( is_admin() ){
     /* theme demo importer */
     include_once( get_template_directory() . '/admin/tz-importer.php' );
 }
+
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
+
+function new_loop_shop_per_page( $cols ) {
+    // $cols contains the current number of products per page based on the value stored on Options -> Reading
+    // Return the number of products you wanna show per page.
+    $cols = 3;
+    return $cols;
+}
 ?>
