ee52139734c43a66705cb092c8bfa21047203f84
thanhtung <tranthanhtung0709@gmail.com>
2017-08-22 16:58:52 +0700

    Added: goolge tag maneger

--- wp-content/themes/interiart/header.php
+++ wp-content/themes/interiart/header.php
@@ -28,6 +28,13 @@
 
     <?php wp_head(); ?>
     <script lang="javascript">(function() {var pname = ( (document.title !='')? document.title : document.querySelector('h1').innerHTML );var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async=1; ga.src = '//live.vnpgroup.net/js/web_client_box.php?hash=a4dfd1099734b76ab13dcd859e17e2d8&data=eyJzc29faWQiOjQ5NzE4MjYsImhhc2giOiIzY2QwOTZhZGUzMWRhOGQwYjc3MzQ0ODFiZWQwZDdkZCJ9&pname='+pname;var s = document.getElementsByTagName('script');s[0].parentNode.insertBefore(ga, s[0]);})();</script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P7PRPVK');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body id="bd" <?php body_class(); ?>>
 <?php $homepage_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
